d798312a3d207b93cad0a9eceb4a35065eb40937
Ivan Gorovoy <ivangorovoyyy@gmail.com>
2019-09-21 08:57:37 +0700

    complete chapter 4

--- app.js
+++ app.js
@@ -1,5 +1,67 @@
 const chalk = require("chalk");
-const getNotes = require("./notes");
+const yargs = require("yargs");
+const { addNote, removeNote, listNotes, readNote } = require("./notes");
 
-console.log(getNotes());
-console.log(chalk.green.inverse.bold("Helldso"));
+// Customize yargs version
+yargs.version("1.1.0");
+
+// add, remove, read, list
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler(argv) {
+    addNote(argv.title, argv.body);
+  }
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  builder: {
+    title: {
+      describe: "Note title to delete",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler(argv) {
+    removeNote(argv.title);
+  }
+});
+
+yargs.command({
+  command: "list",
+  describe: "List a notes",
+  handler() {
+    listNotes();
+  }
+});
+
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  builder: {
+    title: {
+      describe: "Read a note",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler(argv) {
+    readNote(argv.title);
+  }
+});
+
+yargs.parse();
